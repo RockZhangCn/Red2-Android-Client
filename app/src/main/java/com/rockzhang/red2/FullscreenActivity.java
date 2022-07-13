a9d51e1ab7096f1bf4352832bc7ed89d3fc11003
rock.zhang.cn <rock.zhang.cn@gmail.com>
2022-07-13 09:17:15 +0800

    Move login into fullscreen activity, more layout and variables

--- app/src/main/java/com/rockzhang/red2/FullscreenActivity.java
+++ app/src/main/java/com/rockzhang/red2/FullscreenActivity.java
@@ -1,8 +1,14 @@
 package com.rockzhang.red2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +17,9 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowInsets;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rockzhang.red2.log.VLog;
 import com.rockzhang.red2.model.UIPanel;
@@ -19,6 +28,9 @@ import com.rockzhang.red2.presenter.IClientGamePresenter;
 import com.rockzhang.red2.view.IGameView;
 import com.rockzhang.red2.view.PokerView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,6 +44,16 @@ public class FullscreenActivity extends AppCompatActivity implements IGameView, 
     private IClientGamePresenter mPresenter;
 
     private PokerView mBottomPokerView;
+    private PokerView mCenterPokerView;
+
+    private Button mStartGameButton;
+    private Button mDoActionButton;
+    private Button mDoNegativeButton;
+    private TextView mBottomMessage;
+
+    private List<UIPanel> mUIPanelList = new ArrayList<>(4);
+
+    Handler mUIHandler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onResume() {
@@ -55,11 +77,10 @@ public class FullscreenActivity extends AppCompatActivity implements IGameView, 
             actionBar.hide();
         }
 
-        mBottomPokerView = (PokerView) findViewById(R.id.bottom_poker_view);
         mContentView = findViewById(R.id.content_view);
         if (Build.VERSION.SDK_INT >= 30) {
             mContentView.getWindowInsetsController().hide(
-            WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
         } else {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
@@ -72,12 +93,61 @@ public class FullscreenActivity extends AppCompatActivity implements IGameView, 
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
 
-        mPresenter = ClientGame.getInstance();
-        mPresenter.setUIView(this);
+        mBottomPokerView = (PokerView) findViewById(R.id.bottom_poker_view);
+        mBottomMessage = (TextView) findViewById(R.id.bottom_message);
+
+        // Left Pannel
+        TextView leftMessage = (TextView) findViewById(R.id.left_user_message);
+        TextView leftName = (TextView) findViewById(R.id.left_user_name);
+        ImageView leftTimer = (ImageView) findViewById(R.id.left_user_timer_img);
+
+        // Top Pannel
+        TextView topName = (TextView) findViewById(R.id.top_user_name);
+        TextView topMessage = (TextView) findViewById(R.id.top_user_message);
+        ImageView topTimer = (ImageView) findViewById(R.id.top_user_timer_img);
+
+        // Right Pannel
+        TextView rigthName = (TextView) findViewById(R.id.right_user_name);
+        TextView rightMessage = (TextView) findViewById(R.id.right_user_message);
+        ImageView rightTimer = (ImageView) findViewById(R.id.right_user_timer_img);
+
+        // Bottom Pannel
+
+        mCenterPokerView = (PokerView) findViewById(R.id.center_poker_view);
+
+        mStartGameButton = (Button) findViewById(R.id.start_game_item);
+        mDoActionButton = (Button) findViewById(R.id.do_action_button);
+        mDoNegativeButton = (Button) findViewById(R.id.do_negative_button);
 
         Intent startIntent = getIntent();
         mPlayerName = startIntent.getStringExtra("player_name");
         mServerAddress = startIntent.getStringExtra("server_address");
+
+        mPresenter = ClientGame.getInstance();
+        mPresenter.setUIView(this);
+        mPresenter.login(mServerAddress, mPlayerName);
+
+        UIPanel bottom = new UIPanel() {
+            @Override
+            public void showName(String name) {
+
+            }
+
+            @Override
+            public void showMessage(String message, boolean shouldDialog) {
+
+            }
+
+            @Override
+            public void showPokers(List<Integer> cards) {
+
+            }
+
+            @Override
+            public void showTimer(Boolean show) {
+
+            }
+        }
 
         VLog.info("PlayerName is " + mPlayerName + " Server address is " + mServerAddress);
     }
@@ -92,12 +162,46 @@ public class FullscreenActivity extends AppCompatActivity implements IGameView, 
     }
 
     @Override
-    public List<UIPanel> getUIPanelList() {
-        return null;
+    public void OnLoginResult(boolean success, String message) {
+        VLog.info("FullscreenActivity OnLoginResult success " + success + " Message is " + message);
+        mUIHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (!success) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(FullscreenActivity.this);
+                    builder.setTitle("Info");
+                    builder.setMessage(message);
+                    builder.setIcon(R.drawable.icon);
+                    //点击对话框以外的区域是否让对话框消失
+                    builder.setCancelable(true);
+                    //设置正面按钮
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+//                    //设置反面按钮
+//                    builder.setNegativeButton("不是", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.dismiss();
+//                        }
+//                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    mBottomMessage.setText(message);
+                }
+            }
+        });
+
     }
 
     @Override
-    public void OnLoginResult(boolean success, String message) {
-
+    public List<UIPanel> getUIPanelList() {
+        return mUIPanelList;
     }
+
 }
--- app/src/main/java/com/rockzhang/red2/MainActivity.java
+++ app/src/main/java/com/rockzhang/red2/MainActivity.java
@@ -22,15 +22,14 @@ import com.rockzhang.red2.view.IGameView;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements IGameView, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String mServerAddress;
     private String mPlayerName;
     private EditText mServerAddressWidget;
     private EditText mPlayerNameWidget;
-    private IClientGamePresenter mPresenter;
 
-    Handler mUIHandler = new Handler(Looper.getMainLooper());
+
     private Button mStartGame;
 
     @Override
@@ -42,9 +41,6 @@ public class MainActivity extends AppCompatActivity implements IGameView, View.O
         mServerAddressWidget = findViewById(R.id.server_address);
         mPlayerNameWidget = findViewById(R.id.player_name);
         mStartGame.setOnClickListener(this);
-
-        mPresenter = ClientGame.getInstance();
-        mPresenter.setUIView(this);
 
         mServerAddress = (String) SPUtils.get(this, "server_address", "");
         mPlayerName = (String) SPUtils.get(this, "player_name", "");
@@ -66,8 +62,6 @@ public class MainActivity extends AppCompatActivity implements IGameView, View.O
                 mServerAddress = mServerAddressWidget.getText().toString();
                 mPlayerName = mPlayerNameWidget.getText().toString();
 
-                mPresenter.login(mServerAddress, mPlayerName);
-
                 SPUtils.put(this, "server_address", mServerAddress);
                 SPUtils.put(this, "player_name", mPlayerName);
 
@@ -80,53 +74,6 @@ public class MainActivity extends AppCompatActivity implements IGameView, View.O
         }
     }
 
-    @Override
-    public List<UIPanel> getUIPanelList() {
-        return null;
-    }
 
-    @Override
-    public void OnLoginResult(boolean success, String message) {
-        VLog.info("MainActivity OnLoginResult success " + success + " Message is " + message);
-        mUIHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (!success) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Info");
-                    builder.setMessage(message);
-                    builder.setIcon(R.drawable.icon);
-                    //点击对话框以外的区域是否让对话框消失
-                    builder.setCancelable(true);
-                    //设置正面按钮
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-//                    //设置反面按钮
-//                    builder.setNegativeButton("不是", new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            dialog.dismiss();
-//                        }
-//                    });
-
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                } else {
-//                    Intent intent = new Intent(MainActivity.this, FullscreenActivity.class);
-//                    intent.putExtra("server_address", mServerAddress);
-//                    SPUtils.put(MainActivity.this, "server_address", mServerAddress);
-//                    intent.putExtra("player_name", mPlayerName);
-//                    SPUtils.put(MainActivity.this, "player_name", mPlayerName);
-//
-//                    startActivity(intent);
-                }
-            }
-        });
-
-    }
 
 }
